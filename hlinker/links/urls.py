875a546765ab17353da8a8db8ad762232f8397a9
HerbyZ <49283659+HerbyZ@users.noreply.github.com>
2020-12-19 16:05:14 +0300

    Auth: Login + some HTML & CSS

--- hlinker/links/urls.py
+++ hlinker/links/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import link_list_view, create_link_view, create_link_page_view, link_view
+from .views import (
+    link_list_view, create_link_view,
+    create_link_page_view, link_view,
+    delete_link_view, login_page_view,
+    login_view, register_page_view,
+    logout_view, register_view
+)
 
 app_name = 'links'
 urlpatterns = [
@@ -8,4 +14,10 @@ urlpatterns = [
     path('link/<str:link_url>', link_view, name='link'),
     path('new/', create_link_page_view, name='new_link'),
     path('create/', create_link_view, name='create_link'),
+    path('delete/<str:link_url>', delete_link_view, name='delete_link'),
+    path('login/', login_page_view, name='login_page'),
+    path('login/l/', login_view, name='login'),
+    path('register/', register_page_view, name='register_page'),
+    path('register/r', register_view, name='register'),
+    path('logout/', logout_view, name='logout')
 ]
--- hlinker/links/views.py
+++ hlinker/links/views.py
@@ -1,14 +1,23 @@
+from django.contrib import auth
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import login, authenticate, logout
 from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
-from .models import Link
+import string
+import random
+
+from .models import Link, User
 
 
 def link_view(request, link_url):
     try:
         link = Link.objects.get(short_link=link_url)
-    except Exception as e:
-        raise Http404('Link not found.')
+    except ObjectDoesNotExist as e:
+        return render(request, 'error.html', {
+            'error_code': 404,
+            'error_message': 'Link not found.'
+        })
 
     link.follow_count += 1
     link.save()
@@ -42,14 +51,133 @@ def create_link_view(request):
     user = request.user
     data = request.POST
 
-    if data['description'] == None:
-        pass
-    
-    user.link_set.create(
-        name=data['name'],
-        description=data['description'],
-        parent_link=data['parent_link'],
-        short_link='herby'
-    )
+    def generate_url():
+        def create_url():
+            url = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            return url
+
+        url = create_url()
+
+        def check():
+            try:
+                link = Link.objects.get(short_link=url)
+            except ObjectDoesNotExist:
+                return 1
+
+        c = check()
+        while True:
+            url = create_url()
+            if check() == 1:
+                return url
+
+    try:
+        if data['description'] == None:
+                user.link_set.create(
+                name=data['name'],
+                parent_link=data['parent_link'],
+                short_link='herby'
+            )
+        else:
+            user.link_set.create(
+                name=data['name'],
+                description=data['description'],
+                parent_link=data['parent_link'],
+                short_link=generate_url()
+            )
+    except Exception as e:
+        return render(request, 'error.html', {
+            'error_code': 404,
+            # 'error_message': 'All field are required!'
+            'error_message': repr(e)
+        })
 
     return HttpResponseRedirect('/links')
+
+
+def delete_link_view(request, link_url):
+    try:
+        link = Link.objects.get(short_link=link_url)
+        link.delete()
+    except ObjectDoesNotExist:
+        return render(request, 'error.html', {
+            'error_code': 404,
+            'error_message': 'Link not found.'
+        })
+
+    return HttpResponseRedirect('/links')
+
+
+def login_page_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'You already authenticated.'
+        })
+
+    return render(request, 'links/login.html')
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'You already authenticated.'
+        })
+
+    data = request.POST
+
+    try:
+        username = data['username']
+        password = data['password']
+        account = authenticate(username=username, password=password)
+        login(request, account)
+        return HttpResponseRedirect('/')
+    except Exception as e:
+        return render(request, 'error.html', {
+            'error_code': 404,
+            'error_message': 'Wrong login or password'
+        })
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'You already authenticated.'
+        })
+
+    data = request.POST
+
+    try:
+        email = data['email']
+        username = data['username']
+        password = data['password']
+        User.objects.create_user(email, username, password)
+    except:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'Something went wrong or some required field is empty (all fields are required).'
+        })
+
+    return HttpResponseRedirect('/')
+
+
+def register_page_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'You already authenticated.'
+        })
+    
+    return render(request, 'links/register.html')
+
+
+def logout_view(request):
+    if not request.user.is_authenticated:
+        return render(request, 'error', {
+            'error_code': 404,
+            'error_message': 'You aren\'t authenticated.'
+        })
+
+    logout(request)
+    return HttpResponseRedirect('/')
